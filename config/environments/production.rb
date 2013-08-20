76afe1e34591ac1028a8a8391c840d566dee463d
Young <young.cha@hyfn.com>
2013-08-19 22:41:00 -0700

    set serve_static_assets to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,7 @@ SampleApp::Application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
+  config.serve_static_assets = true
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
