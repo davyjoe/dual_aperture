5d9e2aed37811a0133e7e7cc09cc02dd4f4bfc7e
Young <young.cha@hyfn.com>
2013-09-15 20:37:45 -0700

    added static image

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,25 +4,8 @@ class StaticPagesController < ApplicationController
   end
 
   def about
-    @map = GoogleStaticMap.new(zoom: 10, width: 600)
-    @map.markers << MapMarker.new(color: "blue", location: MapLocation.new(address: "890 Robb Rd, Palo Alto, CA 94306"))
   end
 
   def features
-  end
-
-  private 
-  
-  def static_map_for(location, options = {})
-    params = {
-      :center => [location.lat, location.lng].join(","),
-      :zoom => 15,
-      :size => "300x300",
-      :markers => [location.lat, location.lng].join(","),
-      :sensor => true
-      }.merge(options)
- 
-    query_string =  params.map{|k,v| "#{k}=#{v}"}.join("&")
-    image_tag "http://maps.googleapis.com/maps/api/staticmap?#{query_string}", :alt => location.name
   end
 end
