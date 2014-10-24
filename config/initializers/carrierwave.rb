1bd5b2c9273d7306d7709cce363d0a0053eab4ec
Young <young.cha@hyfn.com>
2014-10-23 22:53:56 -0700

    fix carrierwave initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAJK6PIRQAWRUXFDCQ',                        # required
-    :aws_secret_access_key  => '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',                        # required
-    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
-    :host                   => 's3.example.com',             # optional, defaults to nil
-    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']                        # required
   }
-  config.fog_directory  = 'dual-aperture'                          # required
+  config.fog_directory  = ENV['S3_BUCKET_NAME']                         # required
   config.fog_public     = false                                        # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>"max-age=#{365.day.to_i}"} # optional, defaults to {}
 end
