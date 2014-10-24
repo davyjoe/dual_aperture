b80948644c9c170b11c06fb38a5cb098dc26ebee
Young <young.cha@hyfn.com>
2014-10-23 22:49:07 -0700

    added s3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => 'AKIAJK6PIRQAWRUXFDCQ',                        # required
+    :aws_secret_access_key  => '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',                        # required
+    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+    :host                   => 's3.example.com',             # optional, defaults to nil
+    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = 'dual-aperture'                          # required
+  config.fog_public     = false                                        # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>"max-age=#{365.day.to_i}"} # optional, defaults to {}
+end
--- lib/tasks/create_team_members.rake
+++ lib/tasks/create_team_members.rake
@@ -1,9 +1,5 @@
 desc "Create team members"
 task :create_team_members => :environment do 
   data = YAML.load_file(Rails.root + 'config/team_members.yml')
-  data.each do |attributes| 
-    member = TeamMember.new(attributes)
-    member.picture = File.open(File.join(Rails.root, 'app', 'assets', 'images', attributes['picture']))
-    member.save
-  end
+  data.each {|attributes| TeamMember.create(attributes)}
 end
